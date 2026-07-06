482d3215b6622c3c7f09c81997812446dd3ae7a2
Blazzy <1054907322@qq.com>
2026-07-06 20:55:00 +0800

    Fix AO half-res issue that caused waving bandings

--- project/src/engine/Shaders/Deferred/AmbientOcclusionDebug.hlsl
+++ project/src/engine/Shaders/Deferred/AmbientOcclusionDebug.hlsl
@@ -5,7 +5,7 @@ Texture2D<float4> SceneGBufferD : register(t3);
 float4 PSMain(VSFullscreenOutput input) : SV_Target0
 {
     const uint debug_view = (uint)round(AshAOParams2.x);
-    const float depth = SceneDepth.SampleLevel(ScenePointClampSampler, input.uv, 0);
+    const float depth = AshAOSampleSceneDepth(input.uv);
     if (AshAOSceneDepthIsBackground(depth))
     {
         return float4(0.0, 0.0, 0.0, 1.0);
@@ -25,14 +25,14 @@ float4 PSMain(VSFullscreenOutput input) : SV_Target0
 
     if (debug_view == 4u)
     {
-        const float4 gbuffer_e = SceneGBufferE.SampleLevel(ScenePointClampSampler, input.uv, 0);
+        const float4 gbuffer_e = AshAOSampleSceneGBufferE(input.uv);
         const float3 normal = AshAODecodeNormalOct(gbuffer_e.rg);
         return float4(normal * 0.5 + 0.5, 1.0);
     }
 
     if (debug_view == 5u)
     {
-        const float4 motion = SceneGBufferD.SampleLevel(ScenePointClampSampler, input.uv, 0);
+        const float4 motion = SceneGBufferD.SampleLevel(ScenePointClampSampler, AshAOAdjustedSceneUv(input.uv), 0);
         const float2 encoded_motion = saturate(motion.xy * 32.0 + 0.5);
         return float4(encoded_motion, saturate(motion.a), 1.0);
     }
